189d166fb40776bef07fc23f1cbd27e57d590c6a
Nick Pleatsikas <nick@pleatsikas.me>
2023-12-29 16:59:34 -0800

    Added export/import functionality.
    This commit includes the "MVP" version of the tool. This tool can be
    used to upgrade DuckDB database, although it hasn't been tested on every
    platform yet.
    
    - Added function to export or import DuckDB database using downloaded
      binary. This takes several paths and an operation enumeration type
      that tells the function to use the `EXPORT DATABASE` or `IMPORT
      DATABASE` statement.
    - Added code to run function to first export the database to a temporary
      directory and them import it again into a new database file while
      backing up the old file. If the user asks not to back up the database,
      the old file is simply removed after the export step. Once this is
      complete, any temporary files and directories are cleaned up.

--- src/duckdb_upgrade/__main__.py
+++ src/duckdb_upgrade/__main__.py
@@ -1,8 +1,11 @@
 import argparse
+import shutil
+import subprocess
 import requests
 import tempfile
 import zipfile
 
+from enum import StrEnum
 from io import BytesIO
 from packaging.version import Version
 from pathlib import Path
@@ -31,6 +34,25 @@ def get_executable(url: str, version: Version) -> Path:
     return binary_path
 
 
+class DuckDBOperation(StrEnum):
+    Export = "EXPORT"
+    Import = "IMPORT"
+
+
+def run_duckdb_migration_command(
+    op: DuckDBOperation, binary: Path, db: Path, tempdir: Path
+) -> None:
+    result = subprocess.run(
+        [binary, "-c", f"{str(op)} DATABASE '{tempdir}'", db], capture_output=True
+    )
+
+    if result.returncode != 0:
+        raise RuntimeError(
+            f"Failed to {str(op).lower()} database '{db}'."
+            + f"\nstdout:\n{result.stdout}\n\nstderr:\n{result.stderr}"
+        )
+
+
 def run(args: argparse.Namespace) -> None:
     current_storage_version = get_duckdb_version(args.database)
 
@@ -46,17 +68,38 @@ def run(args: argparse.Namespace) -> None:
         )
 
     current_version = VERSION_LOOKUP.latest(current_storage_version)
-    current_version_bin_path = get_executable(
-        VERSION_LOOKUP.get_download_url(current_version), current_version
-    )
-    target_version_bin_path = get_executable(
-        VERSION_LOOKUP.get_download_url(args.target), args.target
-    )
+    current_version_bin_path, target_version_bin_path, export_path = None, None, None
 
-    # TODO: using current version binary to extract the database, save it to a temporary directory,
-    # and then re-import it using the target version binary.
-    #
-    # TODO: add code to make back up of existing database unless user asks not to.
+    try:
+        current_version_bin_path = get_executable(
+            VERSION_LOOKUP.get_download_url(current_version), current_version
+        )
+        target_version_bin_path = get_executable(
+            VERSION_LOOKUP.get_download_url(args.target), args.target
+        )
+
+        export_path = Path(tempfile.mkdtemp(prefix=f"duckdb_export"))
+        run_duckdb_migration_command(
+            DuckDBOperation.Export, current_version_bin_path, args.database, export_path
+        )
+
+        # Unless explicitly told otherwise, back up the file in the same directory with
+        # the '.bak' suffix.
+        if not args.no_backup:
+            args.database.replace(
+                args.database.with_suffix(f"{''.join(args.database.suffixes)}.bak")
+            )
+        else:
+            args.database.unlink()
+
+        run_duckdb_migration_command(
+            DuckDBOperation.Import, target_version_bin_path, args.database, export_path
+        )
+    finally:
+        # Always remove temporary directories.
+        for dir in [current_version_bin_path, target_version_bin_path, export_path]:
+            if dir:
+                shutil.rmtree(dir, ignore_errors=True)
 
 
 def main() -> None:
